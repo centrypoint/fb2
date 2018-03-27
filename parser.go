eeb4df2bde7c88ebe0427af8c4cdb01a772ec8ae
goncharovnikita <goncharovnikita.dev@gmail.com>
2018-03-27 07:44:39 +0300

    add decoder from reader method

--- parser.go
+++ parser.go
@@ -9,13 +9,21 @@ import (
 
 // Parser struct
 type Parser struct {
-	book []byte
+	book   []byte
+	reader io.Reader
 }
 
 // New creates new Parser
 func New(data []byte) *Parser {
 	return &Parser{
 		book: data,
+	}
+}
+
+// NewReader creates new Parser from reader
+func NewReader(data io.Reader) *Parser {
+	return &Parser{
+		reader: data,
 	}
 }
 
@@ -30,6 +38,17 @@ func (p *Parser) CharsetReader(c string, i io.Reader) (r io.Reader, e error) {
 
 // Unmarshal parse data to FB2 type
 func (p *Parser) Unmarshal() (result FB2, err error) {
+	if p.reader != nil {
+		decoder := xml.NewDecoder(p.reader)
+		decoder.CharsetReader = p.CharsetReader
+		if err = decoder.Decode(&result); err != nil {
+			return
+		}
+
+		result.UnmarshalCoverpage(p.book)
+
+		return
+	}
 	reader := bytes.NewReader(p.book)
 	decoder := xml.NewDecoder(reader)
 	decoder.CharsetReader = p.CharsetReader
